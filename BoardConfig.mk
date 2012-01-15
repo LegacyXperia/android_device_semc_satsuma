ea6bcf5bc2306f955fb9c261587f2b2614e3fd3f
freexperia <jerpelea@gmail.com>
2012-01-15 13:10:15 +0200

    semc_satsuma: updated device with latest changes

---
+++ BoardConfig.mk
@@ -0,0 +1,4 @@
+-include device/semc/mogami-common/BoardConfigCommon.mk
+-include vendor/semc/satsuma/BoardConfigVendor.mk
+
+TARGET_OTA_ASSERT_DEVICE := ST17a,ST17i,satsuma
---
+++ cm.mk
@@ -0,0 +1,20 @@
+## Specify phone tech before including full_phone
+$(call inherit-product, vendor/cm/config/gsm.mk)
+
+# Release name
+PRODUCT_RELEASE_NAME := ST17i
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/semc/satsuma/device_satsuma.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := satsuma
+PRODUCT_NAME := cm_satsuma
+PRODUCT_BRAND := SEMC
+PRODUCT_MODEL := Xperia Active
+
+#Set build fingerprint / ID / Product Name ect.
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=ST17i BUILD_ID=ITL41F BUILD_DISPLAY_ID=ITL41F BUILD_FINGERPRINT="google/yakju/maguro:4.0.1/ITL41F/228551:user/release-keys" PRIVATE_BUILD_DESC="yakju-user 4.0.1 ITL41F 228551 release-keys"
