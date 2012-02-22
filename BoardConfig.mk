761938a5a33d29a614b3aba0a27e2020dc8df120
freexperia <jerpelea@gmail.com>
2012-02-22 21:50:57 +0200

    semc: initial sensor config

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,4 +1,11 @@
 -include device/semc/mogami-common/BoardConfigCommon.mk
 -include vendor/semc/satsuma/BoardConfigVendor.mk
 
+BUILD SEMC SENSORS := false
+SENSORS_COMPASS_AK897X := true
+SENSORS_ACCEL_BMA150_INPUT := false
+SENSORS_ACCEL_BMA250_INPUT := true
+SENSORS_PROXIMITY_APDS970X := true
+SENSORS_PRESSURE_BMP180 := true
+
 TARGET_OTA_ASSERT_DEVICE := ST17a,ST17i,satsuma
