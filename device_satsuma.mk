3bac6a68bbefa1229981b3b955c468141f55ad44
Jerpelea <jerpelea@gmail.com>
2012-07-19 13:20:24 +0400

    Revert "satsuma: recovery.fstab path fix"
    This reverts commit 3f9291888d309363b2b4ef86a9b8113403fd3e9b

--- device_satsuma.mk
+++ device_satsuma.mk
@@ -29,7 +29,7 @@ PRODUCT_COPY_FILES += \
     device/semc/satsuma/prebuilt/hw_config.sh:system/etc/hw_config.sh \
     device/semc/satsuma/prebuilt/pre_hw_config.sh:root/pre_hw_config.sh \
     device/semc/msm7x30-common/prebuilt/logo_M.rle:root/logo.rle \
-    device/semc/satsuma/recovery.fstab:root/etc/recovery.fstab
+    device/semc/satsuma/recovery.fstab:root/recovery.fstab
 
 
 # semc msm7x30 uses high-density artwork where available
