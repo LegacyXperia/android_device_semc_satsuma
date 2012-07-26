9d4c1f200af14483754001ad185d34957ec7cc62
freexperia <jerpelea@gmail.com>
2012-07-26 19:38:51 +0300

    satsuma: update mk and various jb fixes

--- cm.mk
+++ cm.mk
@@ -1,22 +1,17 @@
+# Inherit device configuration
+$(call inherit-product, device/semc/satsuma/full_satsuma.mk)
+
 ## Specify phone tech before including full_phone
 $(call inherit-product, vendor/cm/config/gsm.mk)
-
-# Release name
-PRODUCT_RELEASE_NAME := ST17i
-
-TARGET_BOOTANIMATION_NAME := vertical-320x480
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-# Inherit device configuration
-$(call inherit-product, device/semc/satsuma/device_satsuma.mk)
+#Set build fingerprint / ID / Product Name ect.
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=ST17i BUILD_FINGERPRINT="SEMC/ST17i_1254-2184/ST17i:4.0.4/4.1.B.0.431/UL5_3w:user/release-keys" PRIVATE_BUILD_DESC="ST17i-user 4.0.4 4.1.B.0.431 UL5_3w test-keys"
+
+TARGET_BOOTANIMATION_NAME := vertical-320x480
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := satsuma
 PRODUCT_NAME := cm_satsuma
-PRODUCT_BRAND := SEMC
-PRODUCT_MODEL := Xperia Active
-
-#Set build fingerprint / ID / Product Name ect.
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=ST17i BUILD_FINGERPRINT="SEMC/ST17i_1254-6360/ST17i:4.0.4/4.1.B.0.431/UL5_3w:user/release-keys" PRIVATE_BUILD_DESC="ST17i-user 4.0.4 4.1.B.0.431 UL5_3w test-keys"
+PRODUCT_DEVICE := satsuma
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -14,4 +14,4 @@
 # limitations under the License.
 #
 
-add_lunch_combo satsuma-eng
+add_lunch_combo cm_satsuma-eng
