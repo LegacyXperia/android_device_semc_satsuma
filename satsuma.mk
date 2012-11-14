1fb586da3e9ac20edf8fbee8be20190493c7a1c6
codeworkx <codeworkx@cyanogenmod.com>
2012-11-14 15:45:49 +0100

    satsuma: device specific part for the new two-stage boot method
    Ported from http://review.cyanogenmod.org/#/c/26440/
    Author: codeworkx <codeworkx@cyanogenmod.com>
    Signed-off-by: Michael Bestas <mikeioannina@gmail.com>
    
    Change-Id: I5b7e5e0735c2438d9abbb6c071f92e23b2bfd745

--- satsuma.mk
+++ satsuma.mk
@@ -19,9 +19,11 @@ PRODUCT_COPY_FILES += \
     device/semc/satsuma/prebuilt/init.semc.usb.rc:root/init.semc.usb.rc \
     device/semc/satsuma/prebuilt/pre_hw_config.sh:root/pre_hw_config.sh \
     device/semc/satsuma/prebuilt/hw_config.sh:system/etc/hw_config.sh \
-    device/semc/msm7x30-common/prebuilt/logo_M.rle:root/logo.rle \
-    device/semc/satsuma/prebuilt/bootrec-device:root/sbin/bootrec-device \
-    device/semc/satsuma/recovery.fstab:root/recovery.fstab
+    device/semc/msm7x30-common/prebuilt/logo_M.rle:root/logo.rle
+
+# Device specific part for two-stage boot
+PRODUCT_COPY_FILES += \
+    device/semc/satsuma/recovery/bootrec-device:recovery/bootrec-device
 
 # Device specific configs
 PRODUCT_COPY_FILES += \
