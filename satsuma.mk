62dc2a32206ae2c300d962e661172e97d4b2a31f
Michael Bestas <mikeioannina@gmail.com>
2013-04-30 04:37:55 +0300

    satsuma: remove bootrec-device
    Signed-off-by: Michael Bestas <mikeioannina@gmail.com>
    
    Change-Id: I19bfd132c86604b8e5741e77ea238d2c64ae5e80

--- satsuma.mk
+++ satsuma.mk
@@ -7,22 +7,18 @@ $(call inherit-product, device/semc/mogami-common/mogami.mk)
 
 $(call inherit-product, device/common/gps/gps_eu_supl.mk)
 
-PRODUCT_AAPT_CONFIG := normal mdpi mdpi
-PRODUCT_AAPT_PREF_CONFIG := mdpi
-
-# Barometer
+# These are the hardware-specific features
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.barometer.xml:system/etc/permissions/android.hardware.sensor.barometer.xml
+
+PRODUCT_AAPT_CONFIG := normal mdpi mdpi
+PRODUCT_AAPT_PREF_CONFIG := mdpi
 
 # Init files
 PRODUCT_COPY_FILES += \
     device/semc/satsuma/prebuilt/init.semc.usb.rc:root/init.semc.usb.rc \
     device/semc/satsuma/prebuilt/hw_config.sh:system/etc/hw_config.sh \
     device/semc/msm7x30-common/prebuilt/logo_M.rle:root/initlogo.rle
-
-# Device specific part for two-stage boot
-PRODUCT_COPY_FILES += \
-    device/semc/satsuma/recovery/bootrec-device:recovery/bootrec-device
 
 # Device specific configs
 PRODUCT_COPY_FILES += \
